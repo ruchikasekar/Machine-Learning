113e162d03018734bb324148190602ee7bbe3524
Ruchika <rsekar@berkeley.edu>
2025-05-24 00:06:10 +0000

    Pending changes exported from your codespace

--- KNN-classification.py
+++ KNN-classification.py
@@ -1,20 +1,22 @@
 
 import numpy as np
 from collections import Counter
-
+'''Computes Euclidean Distance'''
 def euclidian(x1,x2):
   distance = np.sqrt(np.sum((x1-x2)**2))
   return distance
 
-
 class KNN:
   def __init__(self, k=3):
     self.k = k 
+  '''Trains the model with example data'''
   def fit(self,x,y):
     self.x_train = x #x dataset
     self.y_train = y #y dataset
+  
+  '''Returns the majority label after looping over data points'''
   def predict(self, x):
-    predictions = [self.predict_helper(self,i) for i in x] 
+    predictions = [self.predict_helper(i) for i in x] 
     return predictions
   def predict_helper(self,x):
     #compute distance (euclidian)
@@ -25,8 +27,8 @@ class KNN:
     k_closest = [self.y_train[i] for i in k_indices]
 
     #determine majority vote label
-    most_common_label = Counter(k_closest).mostcommon()
-    return most_common_label
+    most_common_label = Counter(k_closest).most_common(1)
+    return most_common_label[0][0]
   
     
     
